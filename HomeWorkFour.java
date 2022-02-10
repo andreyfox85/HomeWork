50b17433a8cd5c960d783b0b314826333da9f899
andreyfox85 <95551046+andreyfox85@users.noreply.github.com>
2022-02-10 20:07:33 +0700

    Update HomeWorkFour.java

--- HomeWorkFour.java
+++ HomeWorkFour.java
@@ -10,22 +10,23 @@ public class HomeWorkFour {
     }
 
     public int[] answers = {4, 3, 1};
-    int rightAnswersCount = 0;
-    int wrongAnswersCount = 0;
+           int rightAnswersCount = 0;
+           int wrongAnswersCount = 0;
     public String[] questions = {"В файл с каким расширением компилируется java-файл?\n" +
             "1. cs\n2. java\n3. rar\n4. class\n5. exe",
             "С помощью какой команды git можно получить полную копию удаленного репозитория?\n" +
                     "1. commit\n2. push\n3. clone\n4. copy",
             "Какой применяется цикл, когда не известно количество итераций?\n" +
-                    "1. while\n2. for\n3. loop"};
-    
-    for (int i = 0; i<questions.length; i++) {
-        System.out.println(questions[i]);
-        int answer = sc.nextInt();
-        if (answer == answers[i]) {
-            rightAnswersCount++;
-        } else {
-            wrongAnswersCount++;
+                    "1. while\n2. for\n3. loop"};{
+        for (int i = 0; i<questions.length; i++) {
+            System.out.println(questions[i]);
+            int answer = sc.nextInt();
+            if (answer == answers[i]) {
+                rightAnswersCount++;
+            } else {
+                wrongAnswersCount++;
+            }
         }
+                        
     }
 }
