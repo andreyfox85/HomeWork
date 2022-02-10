268ab4c149be7af648bac822755ffd4a87b265db
andreyfox85 <95551046+andreyfox85@users.noreply.github.com>
2022-02-10 21:29:01 +0700

    Update HomeWorkFour.java

--- HomeWorkFour.java
+++ HomeWorkFour.java
@@ -1,24 +1,14 @@
 package HWork;
 
 import java.util.Scanner;
-
 public class HomeWorkFour {
-    Scanner sc = new Scanner(System.in);
-
-    public void say (String result) {
-        System.out.println("Результат: " + "правильно " + rightAnswersCount + " неправильно " + wrongAnswersCount);
-    }
-
-    public int[] answers = {4, 3, 1};
-           int rightAnswersCount = 0;
-           int wrongAnswersCount = 0;
-    public String[] questions = {"В файл с каким расширением компилируется java-файл?\n" +
-            "1. cs\n2. java\n3. rar\n4. class\n5. exe",
-            "С помощью какой команды git можно получить полную копию удаленного репозитория?\n" +
-                    "1. commit\n2. push\n3. clone\n4. copy",
-            "Какой применяется цикл, когда не известно количество итераций?\n" +
-                    "1. while\n2. for\n3. loop"};{
-        for (int i = 0; i<questions.length; i++) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Answers answers = new Answers;
+        Questions questions = new Questions();
+        int rightAnswersCount = 0;
+        int wrongAnswersCount = 0;
+        for (int i = 0; i < questions.length; i++) {
             System.out.println(questions[i]);
             int answer = sc.nextInt();
             if (answer == answers[i]) {
@@ -27,6 +17,5 @@ public class HomeWorkFour {
                 wrongAnswersCount++;
             }
         }
-                        
     }
 }
